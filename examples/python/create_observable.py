4de3a8302d0b498dadbd3b2a691152f23672b0c6
Jonathan Hood <jhood06@gmail.com>
2020-04-23 20:31:08 -0500

    Remove python examples. Lets focus on scala for now.

--- examples/python/create_observable.py
+++
@@ -1,9 +0,0 @@
-import rx
-
-source = rx.of("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
-
-source.subscribe(
-    on_next = lambda i: print("Received {0}".format(i)),
-    on_error = lambda e: print("Error Occurred: {0}".format(e)),
-    on_completed = lambda: print("Done!"),
-)
